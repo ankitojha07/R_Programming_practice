b00e30c564d2539dee2316c73aae8b301b480401
Ankit Ojha <ankitojha1409@gmail.com>
2023-02-23 11:22:09 +0530

    Matrix in R

---
+++ matrix.R
@@ -0,0 +1,12 @@
+m1 <- matrix(c(1,2,3,4), nrow=2, ncol = 2, byrow = TRUE)
+print(m1)
+
+m2 <- matrix(c(1,2,3,4,5,6), nrow=3, ncol = 2, byrow = FALSE)
+print(m2)
+
+
+m3 <- matrix(c(1,2,3,4), nrow=2, ncol = 2, byrow = TRUE)
+print(m1)
+
+m4 <- matrix(c(1,2,3,4,5,6,7,8,9), nrow=3, ncol = 3, byrow = FALSE)
+print(m4)
--- nestedFunctions.R
+++ nestedFunctions.R
@@ -14,3 +14,33 @@ m2 <- "kjhgf"
 nchar(m1)
 paste(m1,m2)
 
+emp <- c("Ojha", "asdf", "asdfg", "awe45yhb")
+
+print(emp)
+print(emp[1])
+
+emp[2] <- "Ankit"
+
+print(emp[2])
+
+cat("\n Updated Vector", emp)
+
+num1 <- rep(c(1,2,5,6), times=2)
+print(num1)
+
+num <- rep(c(1,2,5,6), each=2)
+cat(num)
+
+for (n in num1){
+  print(n)
+}
+
+
+i <- 1
+while (i < length(num)) {
+  print(num[i])
+  i <- i + 1
+}
+
+cat(m1[,2])
+
